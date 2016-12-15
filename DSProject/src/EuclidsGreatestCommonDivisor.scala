d64d526bd6de3b54bb89b206eacd071618699205
GowriMrunal <GowriMrunal@192.168.2.11>
2016-12-15 13:00:14 +0530

    Euclids GCD implementation

---
+++ DSProject/src/EuclidsGreatestCommonDivisor.scala
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+
+object EuclidsGreatestCommonDivisor extends App {
+  
+  val scan = new Scanner(System.in)
+  val upper = scan.nextInt()
+  val lower = scan.nextInt()
+  
+  System.out.println(calculateGCD(upper,lower))
+  
+  def calculateGCD(a:Int, b:Int):Int = {
+    
+    if(b == 0) return a;
+    else calculateGCD(b, a%b)
+  }
+  
+}
